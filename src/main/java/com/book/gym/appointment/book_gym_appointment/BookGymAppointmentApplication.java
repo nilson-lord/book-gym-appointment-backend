bc794a15da58d9351075cd4d0e46c27e3580ef2d
Nilson <pumanelca@gmail.com>
2025-05-09 10:52:57 -0500

    book-gym-appointment

---
+++ src/main/java/com/book/gym/appointment/book_gym_appointment/BookGymAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.book.gym.appointment.book_gym_appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookGymAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookGymAppointmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book/gym/appointment/book_gym_appointment/BookGymAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.gym.appointment.book_gym_appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookGymAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
